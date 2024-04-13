306427c05e15029a407f7c0fec2376d947847d43
G.P.Eug <72270592+Deweed@users.noreply.github.com>
2024-04-13 20:56:25 -0300

    Add files via upload

---
+++ LotoFacilGUI/src/LotoFacil.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class LotoFacil {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new LotoFacilGUI().setVisible(true));
+    }
+}
